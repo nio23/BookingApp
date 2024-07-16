124b24ad4583fb0cfd62a07b5f0af58ddc9b6d79
nio23 <47280133+nio23@users.noreply.github.com>
2024-07-16 16:26:44 +0300

    Calendar shows the correct day and number

--- client/src/app/calendar/calendar.component.ts
+++ client/src/app/calendar/calendar.component.ts
@@ -1,6 +1,7 @@
 import { NgFor } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-calendar',
@@ -16,25 +17,31 @@ export class CalendarComponent implements OnInit {
   
   date = new Date();
   year = this.date.getFullYear();
+
+  //Month from 0..11
   monthNumb:number = this.date.getMonth();
   monthStr:string = this.months[this.monthNumb];
-  lastDateofMonth = new Date(this.year, this.monthNumb +1, 0).getDate();
-  daysOfMonth = Array.from({length:this.lastDateofMonth},(_,i)=> i+1);
+  firstDayOfMonth = new Date(this.year, this.monthNumb, 1).getDay();
+  lastDateofMonth = new Date(this.year, this.monthNumb + 1, 0).getDate();
+  //daysOfMonth = Array.from({length:this.lastDateofMonth},(_,i)=> i+1);
   weeks: number = 5;
-  weeksArray:any[] = [];
-
+  weeksArray:any = [];
 
   ngOnInit(): void {
     this.generateCalendar();
   }
 
   generateCalendar(){
+    this.weeksArray = [];
+    let curDay = 1;
     for (let i = 0; i < this.weeks; i++) {
       this.weeksArray.push(Array.from({length: 7}, (_, index) => {
-        var date = i*7+(index+1);
-        return date<this.lastDateofMonth ? date : "";
+        var date = i*7+(index);
+        return ( date >= this.firstDayOfMonth && date<=this.lastDateofMonth) ? curDay++ : "";
       }))
     }
+    console.log(this.firstDayOfMonth);
+    console.log(this.weeksArray);
   }
 
   nextYear(){
@@ -43,6 +50,17 @@ export class CalendarComponent implements OnInit {
 
   prevYear(){
     this.year = this.year-1;
+  }
+
+  changeMonth(month:number){
+    this.monthNumb = month;
+    this.monthStr = this.months[this.monthNumb]
+    this.calcFirstDayOfMonth();
+    this.generateCalendar();
+  }
+
+  calcFirstDayOfMonth(){
+    this.firstDayOfMonth = new Date(this.year, this.monthNumb, 1).getDay();
   }
 
 }
