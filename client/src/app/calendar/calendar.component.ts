4772018a4dc593766cdef3b2999411c41e7434de
nio23 <47280133+nio23@users.noreply.github.com>
2024-07-18 15:30:03 +0300

    Added bootstrap calendar

--- client/src/app/calendar/calendar.component.ts
+++ client/src/app/calendar/calendar.component.ts
@@ -1,12 +1,10 @@
-import { NgFor } from '@angular/common';
-import { Component, computed, effect, inject, OnInit, Signal, signal } from '@angular/core';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { of } from 'rxjs';
+import { Component, computed, effect, OnInit, Signal, signal } from '@angular/core';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [NgFor, BsDropdownModule],
+  imports: [BsDatepickerModule],
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.css'
 })
@@ -23,35 +21,44 @@ export class CalendarComponent implements OnInit {
   weeks: number = 5;
   weeksArray:any = [];
 
-  constructor(){
-    effect(()=>{
-      this.generateCalendar();
-    })
+  // constructor(){
+  //   // effect(()=>{
+  //   //   this.generateCalendar();
+  //   // })
+  // }
+
+  bsInlineValue = new Date();
+  bsInlineRangeValue: Date[];
+  maxDate = new Date();
+ 
+  constructor() {
+    this.maxDate.setDate(this.maxDate.getDate() + 7);
+    this.bsInlineRangeValue = [this.bsInlineValue, this.maxDate];
   }
 
   ngOnInit(): void {
 
   }
 
-  generateCalendar(){
-    let firstDayOfWeek: number =  new Date(this.date().getFullYear(), this.monthNumb(), 1).getDay();
-    let lastDateofMonth: number =  new Date(this.date().getFullYear(), this.monthNumb() + 1, 0).getDate();
-    let lastDateOfPreviousMonth: number = new Date(this.date().getFullYear(), this.monthNumb(), 0).getDate();
-    this.weeksArray = [];
-    let day = 1;
-    let firstDayOfPreviousMonth = lastDateOfPreviousMonth - firstDayOfWeek + 1
-    let firstDayOfNextMonth = 1;
-    for (let i = 0; i < this.weeks; i++) {
-      this.weeksArray.push(Array.from({length: 7}, (_, index) => {
-        let date = i*7+(index);
-        if(date >= firstDayOfWeek && day <= lastDateofMonth)
-          return day++;
-        if(date < firstDayOfWeek)
-          return firstDayOfPreviousMonth++;
-        return firstDayOfNextMonth++;
-      }))
-    }
-  }
+  // generateCalendar(){
+  //   let firstDayOfWeek: number =  new Date(this.date().getFullYear(), this.monthNumb(), 1).getDay();
+  //   let lastDateofMonth: number =  new Date(this.date().getFullYear(), this.monthNumb() + 1, 0).getDate();
+  //   let lastDateOfPreviousMonth: number = new Date(this.date().getFullYear(), this.monthNumb(), 0).getDate();
+  //   this.weeksArray = [];
+  //   let day = 1;
+  //   let firstDayOfPreviousMonth = lastDateOfPreviousMonth - firstDayOfWeek + 1
+  //   let firstDayOfNextMonth = 1;
+  //   for (let i = 0; i < this.weeks; i++) {
+  //     this.weeksArray.push(Array.from({length: 7}, (_, index) => {
+  //       let date = i*7+(index);
+  //       if(date >= firstDayOfWeek && day <= lastDateofMonth)
+  //         return day++;
+  //       if(date < firstDayOfWeek)
+  //         return firstDayOfPreviousMonth++;
+  //       return firstDayOfNextMonth++;
+  //     }))
+  //   }
+  // }
 
   nextYear(){
     this.date.update(value => value = new Date(value.getFullYear()+1, value.getMonth()));
