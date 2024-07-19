f4cb83fe791c591f5c17c563d1d5d4260630db45
nio23 <47280133+nio23@users.noreply.github.com>
2024-07-19 15:23:26 +0300

    Added ngx-datepicker

--- client/src/app/calendar/calendar.component.ts
+++ client/src/app/calendar/calendar.component.ts
@@ -1,5 +1,5 @@
 import { Component, computed, effect, OnInit, Signal, signal } from '@angular/core';
-import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { BsDatepickerModule, BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-calendar',
@@ -9,67 +9,21 @@ import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
   styleUrl: './calendar.component.css'
 })
 export class CalendarComponent implements OnInit {
-
-  months = ["January", "February", "March", "April", "May", "June", "July",
-            "August", "September", "October", "November", "December"];
-  
-  date = signal(new Date());
-  //Month from 0..11
-  monthNumb: Signal<number> = computed(()=> this.date().getMonth());
-  monthStr: Signal<string> = computed(() => this.months[this.monthNumb()]);
-
-  weeks: number = 5;
-  weeksArray:any = [];
-
-  // constructor(){
-  //   // effect(()=>{
-  //   //   this.generateCalendar();
-  //   // })
-  // }
-
+  datePickerConfig: Partial<BsDatepickerConfig>;
   bsInlineValue = new Date();
-  bsInlineRangeValue: Date[];
-  maxDate = new Date();
- 
-  constructor() {
-    this.maxDate.setDate(this.maxDate.getDate() + 7);
-    this.bsInlineRangeValue = [this.bsInlineValue, this.maxDate];
+
+  constructor(){
+    this.datePickerConfig = Object.assign({}, { showWeekNumbers: false})
+  }
+
+
+  onDateSelected(selected: Date){
+    this.bsInlineValue = selected;
+    console.log(`Selected date is ${this.bsInlineValue}`);
   }
 
   ngOnInit(): void {
 
-  }
-
-  // generateCalendar(){
-  //   let firstDayOfWeek: number =  new Date(this.date().getFullYear(), this.monthNumb(), 1).getDay();
-  //   let lastDateofMonth: number =  new Date(this.date().getFullYear(), this.monthNumb() + 1, 0).getDate();
-  //   let lastDateOfPreviousMonth: number = new Date(this.date().getFullYear(), this.monthNumb(), 0).getDate();
-  //   this.weeksArray = [];
-  //   let day = 1;
-  //   let firstDayOfPreviousMonth = lastDateOfPreviousMonth - firstDayOfWeek + 1
-  //   let firstDayOfNextMonth = 1;
-  //   for (let i = 0; i < this.weeks; i++) {
-  //     this.weeksArray.push(Array.from({length: 7}, (_, index) => {
-  //       let date = i*7+(index);
-  //       if(date >= firstDayOfWeek && day <= lastDateofMonth)
-  //         return day++;
-  //       if(date < firstDayOfWeek)
-  //         return firstDayOfPreviousMonth++;
-  //       return firstDayOfNextMonth++;
-  //     }))
-  //   }
-  // }
-
-  nextYear(){
-    this.date.update(value => value = new Date(value.getFullYear()+1, value.getMonth()));
-  }
-
-  prevYear(){
-    this.date.update(value => value = new Date(value.getFullYear()-1, value.getMonth()));
-  }
-
-  changeMonth(selectedMonth:number){
-    this.date.update(value => value = new Date(value.getFullYear(), selectedMonth));
   }
 
 }
