56c250451863c928c25394a6bfba9ab9b17b11b2
Home <nio_dapro@hotmail.com>
2025-02-09 11:54:11 +0200

    Added an api Exception Middleware

--- API/Dtos/UpdateAppointmentDto.cs
+++ API/Dtos/UpdateAppointmentDto.cs
@@ -1,9 +1,12 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.Dtos;
 
 public class UpdateAppointmentDto
 {
+    [Required]
     public int Id { get; set; }
-    public required string Date { get; set; } 
+    [Required]
+    public string Date { get; set; } = string.Empty;
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entities;
 using API.Extensions;
+using API.Middleware;
 using API.SignalR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +15,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
     .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
